e4052150a673fa57c45815e656fb02ba254a6052
Vai <144768865+viywolf@users.noreply.github.com>
2026-05-21 10:52:00 +1200

    Saving this before trying out a different way -v

--- scenes/travellers_manager.gd
+++ scenes/travellers_manager.gd
@@ -4,31 +4,17 @@ var current_turn : int = 0
 
 func _ready() -> void:
 	pass
-	#Engine.physics_ticks_per_second = 6
+	Engine.physics_ticks_per_second = 6
 
 func _physics_process(_delta: float) -> void:
+	if(Input.is_action_just_pressed("ui_accept")):
+		$Random.check_if_enclosed(Vector2i(1,0))
 	current_turn += 1
 	for traveller in get_children():
 		if(current_turn % int(traveller.get_speed()) == 0):
 			traveller.move(traveller.get_next_position())
 
 
-
-
-
-
-
-
-
-
-
 """
 This carries all the global variables such as every traveller position.
-Children can easily access it.
-Also consider moving all the 'move' functions here
-as I'm not sure if it's the best to keep it in the children.
-But we also have to consider things like speed of movement
-and size of traveller.
-
--j
 """
--- scripts/travellers/traveller.gd
+++ scripts/travellers/traveller.gd
@@ -24,10 +24,12 @@ var directions : Array[Vector2i] = [
 
 @export var current_position : Vector2i = Vector2i(0, 0)
 var previous_position : Vector2i = Vector2i(0, 0)
+var next_position : Vector2i = Vector2i(0, 0)
 
 @export_category("Modifiers")
 
 @export var traveller_modifier : String
+@export var disabled : bool = false
 
 func _ready() -> void:
 	if(get_parent() is TileMapLayer):
@@ -35,26 +37,86 @@ func _ready() -> void:
 	else:
 		printerr("No tile map layer to reference!")
 
-func move(next_position : Vector2i) -> void:
+func move(to_next_position : Vector2i) -> void:
+	if(disabled):
+		return
+	next_position = to_next_position
 	if(can_move(next_position)):
 		previous_position = current_position
-		if(tilemap.get_cell_tile_data(next_position).get_custom_data("Colour") == passed_colour):
-			check_if_enclosed()
+		if(tilemap.get_cell_tile_data(next_position) != null 
+		   and tilemap.get_cell_tile_data(next_position).get_custom_data("Colour") == passed_colour):
+			check_if_enclosed(next_position)
 		tilemap.set_cell(next_position, 0, active_colour)
 		tilemap.get_cell_tile_data(next_position).set_custom_data("Colour", active_colour)
 		tilemap.set_cell(current_position, 0, passed_colour)
 		tilemap.get_cell_tile_data(current_position).set_custom_data("Colour", passed_colour)
 		current_position = next_position
 
-func can_move(next_position : Vector2i) -> bool:
-	if(tilemap.get_cell_tile_data(next_position) == null or 
-	   tilemap.get_cell_tile_data(next_position).get_custom_data("Colour") == passed_colour):
+func can_move(to_next_position : Vector2i) -> bool:
+	if(tilemap.get_cell_tile_data(to_next_position) == null or 
+	   tilemap.get_cell_tile_data(to_next_position).get_custom_data("Colour") == passed_colour):
 		return true
 	else:
 		return false
 
-func check_if_enclosed() -> void:
-	pass
+func check_if_enclosed(current_next_position : Vector2i) -> void:
+	var q : Array[Vector2i]
+	var visited : Array[Array]
+	var visited_list : Array[Vector2i]
+	for i in range(500):
+		var a:Array
+		a.resize(500)
+		visited.push_back(a)
+		
+	# Temporary to stop array from going out of range
+	var offset = 250
+		
+	q.push_back(current_next_position)
+	# Make sure we get the empty cell if there is any
+	q.push_back(current_next_position + Vector2i.DOWN)
+	q.push_back(current_next_position + Vector2i.UP)
+	q.push_back(current_next_position + Vector2i.LEFT)
+	q.push_back(current_next_position + Vector2i.RIGHT)
+	var cur_cell : Vector2i
+	print("checking")
+	while(q.is_empty() == false):
+		cur_cell = q.pop_front()
+		var current_cell_data = tilemap.get_cell_tile_data(cur_cell)
+		if(visited[cur_cell.x + offset][cur_cell.y + offset] == true):
+			continue
+		# Doesnt really work, inf loop
+		if((current_cell_data != null
+		   and (current_cell_data.get_custom_data("Colour") != passed_colour
+		   and current_cell_data.get_custom_data("Colour") != active_colour))):
+			print("breaking at " + str(cur_cell))
+			break
+		if(current_cell_data != null and
+			current_cell_data.get_custom_data("Colour") == passed_colour):
+			continue
+		visited[cur_cell.x + offset][cur_cell.y + offset] = true
+		visited_list.push_back(cur_cell)
+		q.push_back(cur_cell + Vector2i.DOWN)
+		q.push_back(cur_cell + Vector2i.UP)
+		q.push_back(cur_cell + Vector2i.LEFT)
+		q.push_back(cur_cell + Vector2i.RIGHT)
+		await get_tree().process_frame
+		if(q.size() > 1000):
+			print("q too big, breaking")
+			break
+	print("done")
+	print(q.size())
+	print(q)
+	print("visited")
+	print(visited_list)
+	if(q.is_empty()):
+		printerr("filling it in!!")
+		for key in visited_list:
+			tilemap.set_cell(key, 0, passed_colour)
+	
+	#temporary
+	if(true):
+		for key in visited_list:
+			tilemap.set_cell(key, 0, passed_colour)
 
 @abstract func get_next_position() -> Vector2i
 
