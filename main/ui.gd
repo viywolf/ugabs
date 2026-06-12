53b27b767274bc1d8306c7611a3701d6e579cff2
Vai <144768865+viywolf@users.noreply.github.com>
2026-06-12 16:43:04 +1200

    Finished speed scaling
    I will hopefully make it prettier later

--- main/ui.gd
+++ main/ui.gd
@@ -3,12 +3,19 @@ extends CanvasLayer
 func _ready() -> void:
 	Engine.physics_ticks_per_second = 12
 
-func _on_speed_slider_drag_ended(value_changed: bool) -> void:
-	if value_changed:
-		print("changed")
-		if $SpeedSlider.value == 0:
-			# Set all to disabled
-			pass
-		else:
-			Engine.physics_ticks_per_second = $SpeedSlider.value
-			print("changed")
+func _on_speed_slider_value_changed(value: float) -> void:
+	var value_to_ticks : int = int(round(value*value))
+	if value_to_ticks == 0:
+		for node in get_tree().current_scene.get_children():
+			if node == TileMapLayer:
+				node.set_physics_process(false)
+		get_tree().paused = true
+		Engine.physics_ticks_per_second = 1
+	else:
+		for node in get_tree().current_scene.get_children():
+			if node == TileMapLayer:
+				node.set_physics_process(true)
+		get_tree().paused = false
+		Engine.physics_ticks_per_second = value_to_ticks
+
+# Process : mode : always/pausable
